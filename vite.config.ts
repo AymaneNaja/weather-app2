65712f57774df6c18bc49f8de83aee6c14ce4c48
fun time <mrjgaming4@gmail.com>
2023-02-21 01:03:58 +0400

    weather2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://AymaneNaja.github.io/weather-app2',
   plugins: [react()]
 })
